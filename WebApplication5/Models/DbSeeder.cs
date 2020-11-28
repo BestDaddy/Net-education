449c7f51571142e531b6f9de52986ecda2668ce8
Kenzhebayev <azik.99.kz@gmail.com>
2020-11-28 18:55:47 +0600

    Добавьте файлы проекта.

---
+++ WebApplication5/Models/DbSeeder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication5.Models
+{
+    public static class DbSeeder
+    {
+        public static void Initialize(MobileContext context)
+        {
+            if (!context.Items.Any())
+            {
+                context.Items.AddRange(
+                    new Item
+                    {
+                        Title = "Car",
+                        Decription = "BMW",
+                        Price = 100
+                    },
+                    new Item
+                    {
+                        Title = "Computer",
+                        Decription = "i5 GTX1660s",
+                        Price = 14
+                    },
+                    new Item
+                    {
+                        Title = "Thing",
+                        Decription = "idk",
+                        Price = 5666
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
